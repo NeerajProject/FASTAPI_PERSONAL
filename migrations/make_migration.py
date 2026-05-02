4bbc773507fb8fca8c3fe7ee7787aa53af09b3dd
Your Name <you@example.com>
2026-05-02 19:17:23 +0400

    added

--- migrations/make_migration.py
+++ migrations/make_migration.py
@@ -1,4 +1,10 @@
-import database  
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from database import Base, engine
 import models
 
-Base.metadata.create_all(bind=engine)
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
+    print("Migrations completed successfully!")
